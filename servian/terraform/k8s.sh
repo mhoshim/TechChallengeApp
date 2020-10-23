4fab70e9bfd9d80f5ea36611bf694f4c91453640
mhoshim <mh.oshim@yahoo.com>
2020-10-23 15:37:39 +0800

    Update k8s.sh

--- servian/terraform/k8s.sh
+++ servian/terraform/k8s.sh
@@ -5,10 +5,10 @@ sed -i '.bak' "s/REPLACE_PROJ_ID/$1/g" terraform.tfvars
 sed -i '.bak' "s/REPLACE_REGION/$2/g" terraform.tfvars
 
 #initialize terraform workspace
-./terraform init
+terraform init
 
 #apply the terraform plan
-./terraform apply
+terraform apply
 
 #retrieve the access credentials for your cluster and automatically configure 'kubectl'
-gcloud container clusters get-credentials $(./terraform output kubernetes_cluster_name) --region $(./terraform output region)-a
+gcloud container clusters get-credentials $(terraform output kubernetes_cluster_name) --region $(terraform output region)-a
